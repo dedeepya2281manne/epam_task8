4122c6e0fd59eeb9ac91718275a222bab235a169
dedeepya <dedeepyamanne02@gmail.com>
2020-03-18 16:46:40 +0530

    Changed code to get more Code Coverage

--- JUnit/src/main/java/com/junit/JUnit/CheckA.java
+++ JUnit/src/main/java/com/junit/JUnit/CheckA.java
@@ -9,25 +9,29 @@ public class CheckA {
 			result = string;
 		}
 		if(stringLength == 2 ){
-			if((char)string.charAt(0) == 'A'&&(char)string.charAt(1) == 'A'){
+			char firstChar = string.charAt(0);
+			char secondChar = string.charAt(1);
+			if(firstChar == 'A' && secondChar == 'A'){
 				return result;
 			}
-			else if((char)string.charAt(0) == 'A'){
+			else if(firstChar == 'A'){
 				result = string.substring(1,2);
 			}
-			else if ((char)string.charAt(1) == 'A'){
+			else if (secondChar == 'A'){
 				result = string.substring(0,1);
 			}
 		}
 		if(stringLength > 2){
-			if((char)string.charAt(0) == 'A' && (char)string.charAt(1) == 'A'){
+			char firstChar = string.charAt(0);
+			char secondChar = string.charAt(1);
+			if(firstChar == 'A' && secondChar == 'A'){
 				result = string.substring(2,stringLength);
 			}
-			else if((char)string.charAt(0) == 'A'){
+			else if(firstChar == 'A'){
 				result = string.substring(1,stringLength);
 			}
-			else if((char)string.charAt(1) == 'A'){
-				result = string.substring(0,1)+string.substring(2,stringLength);
+			else if(secondChar == 'A'){
+				result = firstChar+string.substring(2,stringLength);
 			}
 			else{
 				result = string;
